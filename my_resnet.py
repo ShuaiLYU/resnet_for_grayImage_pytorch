7e4896b2deb00e4ab43ab4abfc4f1baa4b368666
Wslsdx <31230805+Wslsdx@users.noreply.github.com>
2020-04-23 15:09:49 +0800

    Update my_resnet.py

--- my_resnet.py
+++ my_resnet.py
@@ -192,42 +192,11 @@ def resnet50(pretrained=False, progress=True, **kwargs):
 
 
 if __name__=="__main__":
+    
+    # rgb image
+    model = resnet18(pretrained=True,b_RGB=True)
+
+    # gray image
     model = resnet18(pretrained=True,b_RGB=False)
-    model.eval()
-    #
-    # import torch
-    # # Download an example image from the pytorch website
-    # import urllib
-    # url, filename = ("https://github.com/pytorch/hub/raw/master/dog.jpg", "dog.jpg")
-    # try: urllib.URLopener().retrieve(url, filename)
-    # except: urllib.request.urlretrieve(url, filename)
-    #
-    # # sample execution (requires torchvision)
-    # from PIL import Image
-    # from torchvision import transforms
-    # input_image = Image.open(filename)
-    # preprocess = transforms.Compose([
-    #     transforms.Resize(256),
-    #     transforms.CenterCrop(224),
-    #     transforms.ToTensor(),
-    #     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-    # ])
-    # input_tensor = preprocess(input_image)
-    # input_batch = input_tensor.unsqueeze(0) # create a mini-batch as expected by the model
-    #
-    # # move the input and model to GPU for speed if available
-    # if torch.cuda.is_available():
-    #     input_batch = input_batch.to('cuda')
-    #     model.to('cuda')
-    #
-    # with torch.no_grad():
-    #     output = model(input_batch[:,0,:,:])
-    # # Tensor of shape 1000, with confidence scores over Imagenet's 1000 classes
-    # #print(output[0])
-    # # The output has unnormalized scores. To get probabilities, you can run a softmax on it.
-    # #print(torch.nn.functional.softmax(output[0], dim=0))
-    #
-    # result = torch.nn.functional.softmax(output[0], dim=0)
-    # print(result.argmax())
 
 
